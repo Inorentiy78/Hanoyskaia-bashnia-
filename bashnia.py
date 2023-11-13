2240f36b398fe23d4470a3496d1b927720fec150
aiwuio <148534891+aiwuio@users.noreply.github.com>
2023-11-13 15:35:53 +0500

    Update bashnia.py

--- bashnia.py
+++ bashnia.py
@@ -1,108 +1,86 @@
-не закончен
 import pygame
 import sys
 
 # Инициализация Pygame
 pygame.init()
 
-# Цвета
+# Определение цветов
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-RED = (255, 0, 0)
 
-# Размеры окна
-WIDTH, HEIGHT = 800, 400
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+# Определение параметров дисков
+DISK_WIDTH = 30
+DISK_HEIGHT = 20
+
+# Определение параметров башен
+TOWER_WIDTH = 10
+TOWER_HEIGHT = 200
+TOWER_SPACING = 200  # Изменено на более широкий интервал
+
+# Определение параметров окна
+WIDTH = 800  # Увеличено для увеличения ширины столбцов
+HEIGHT = 400
+
+# Инициализация экрана
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Ханойская башня")
 
-# Размеры дисков
-BASE_WIDTH = 100 # Базовая ширина для самого большого диска
-DISK_HEIGHT = 20
-DISK_SPACING = 5
+class Disk:
+    def __init__(self, size, color):
+        self.size = size
+        self.color = color
 
-# Словарь для хранения текущих позиций дисков
-disk_positions = {
-    "A": [],
-    "B": [],
-    "C": [],
-    "D": [],
-    "E": [],
-    "F": []
-}
+# Определение дисков и башен
+towers = [[Disk(5, (255, 0, 0)), Disk(4, (255, 255, 0)), Disk(3, (0, 255, 0)), Disk(2, (0, 255, 255)), Disk(1, (0, 0, 255))], [], []]
 
-# Функция для создания начальных позиций дисков на башнях
-def initialize_positions():
-    for tower, disks in towers.items():
-        x = 0
-        if tower == "A":
-            x = WIDTH // 12
-        elif tower == "B":
-            x = WIDTH // 12 * 3
-        elif tower == "C":
-            x = WIDTH // 12 * 5
-        elif tower == "D":
-            x = WIDTH // 12 * 7
-        elif tower == "E":
-            x = WIDTH // 12 * 9
-        elif tower == "F":
-            x = WIDTH // 12 * 11
+# Определение функции отрисовки башен и дисков
+def draw_towers():
+    for i, tower in enumerate(towers):
+        x = i * TOWER_SPACING + (WIDTH - 3 * TOWER_SPACING) // 2  # Разместить столбцы в центре
+        pygame.draw.rect(screen, BLACK, (x, HEIGHT - TOWER_HEIGHT, TOWER_WIDTH, TOWER_HEIGHT))
+        for j, disk in enumerate(tower):
+            disk_width = disk.size * DISK_WIDTH
+            disk_height = DISK_HEIGHT
+            pygame.draw.rect(screen, disk.color, (x - disk_width // 2 + TOWER_WIDTH // 2, HEIGHT - (j + 1) * disk_height, disk_width, disk_height))
 
-        y = HEIGHT - DISK_HEIGHT
-        for disk in disks:
-            disk_positions[tower].append((x - disk * 2, y, BASE_WIDTH + disk * 4, DISK_HEIGHT))
-            y -= DISK_HEIGHT + DISK_SPACING
+# Определение функции перемещения диска
+def move_disk(source, target):
+    if towers[source] and (not towers[target] or towers[source][-1].size < towers[target][-1].size):
+        disk = towers[source].pop()
+        towers[target].append(disk)
 
-# Функция отрисовки башен и дисков
-def draw():
-    WIN.fill(WHITE)
+# Определение главного цикла игры
+def main():
+    clock = pygame.time.Clock()
 
-    for tower, disks in towers.items():
-        for pos in disk_positions[tower]:
-            pygame.draw.rect(WIN, RED, pos)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-    pygame.display.update()
+            # Обработка событий клавиш
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    move_disk(0, 1)
+                elif event.key == pygame.K_2:
+                    move_disk(0, 2)
+                elif event.key == pygame.K_3:
+                    move_disk(1, 0)
+                elif event.key == pygame.K_4:
+                    move_disk(1, 2)
+                elif event.key == pygame.K_5:
+                    move_disk(2, 0)
+                elif event.key == pygame.K_6:
+                    move_disk(2, 1)
 
-towers = {
-    "A": [i for i in range(6, 0, -1)],
-    "B": [],
-    "C": [],
-    "D": [],
-    "E": [],
-    "F": []
-}
+        # Отрисовка
+        screen.fill(WHITE)
+        draw_towers()
+        pygame.display.flip()
 
-initialize_positions()
+        # Ограничение частоты кадров
+        clock.tick(30)
 
-selected_tower = None
-selected_disk = None
-
-# Основной цикл игры
-running = True
-while running:
-    draw()
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            x, y = pygame.mouse.get_pos()
-            for tower, pos_list in disk_positions.items():
-                for pos in pos_list:
-                    disk_rect = pygame.Rect(pos)
-                    if disk_rect.collidepoint(x, y):
-                        if not selected_tower:
-                            selected_tower = tower
-                            selected_disk = towers[tower].pop()
-                            break
-                        else:
-                            if (not towers[tower] or towers[tower][-1] > selected_disk) and tower != selected_tower:
-                                towers[tower].append(selected_disk)
-                            else:
-                                towers[selected_tower].append(selected_disk)
-                            selected_tower = None
-                            selected_disk = None
-
-    pygame.display.update()
-
-pygame.quit()
-sys.exit()
+if __name__ == "__main__":
+    main()
