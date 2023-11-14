d9ba4f7ff7d790e5850dae4a8884352a5ff09316
Inokentiy78 <148535452+Inorentiy78@users.noreply.github.com>
2023-11-14 13:28:41 +0500

    Update bashnia.py

--- bashnia.py
+++ bashnia.py
@@ -60,27 +60,46 @@ def create_towers(num_disks):
     ]
 
 # Функция для отрисовки кнопок с числом дисков
-def draw_disk_button(num_disks):
+def draw_disk_button(num_disks, is_start_button):
     font = pygame.font.Font(None, 35)
     text = font.render("Количество дисков", True, BLACK)
-    pygame.draw.rect(screen, (200, 200, 200), (50, 50, 250, 50))  # Button-like appearance
-    screen.blit(text, (60, 60))
     text_num = font.render(str(num_disks), True, BLACK)
-    screen.blit(text_num, ( 320, 60)) 
+
+    text_width = text.get_width() + text_num.get_width() + 20  # Calculate total width for the text and spacing
+    button_rect = pygame.Rect(50, 50, text_width, 50)  # Use calculated text width
+
+    pygame.draw.rect(screen, (200, 200, 200), button_rect)
+    screen.blit(text, (button_rect.x + 10, 60))  # Adjust text position based on the button rectangle
+    screen.blit(text_num, (button_rect.right - text_num.get_width() - 10, 60))  # Adjust the 'num_disks' position
+
+    if is_start_button:
+        start_text = font.render("Пуск", True, BLACK)
+        start_button_rect = pygame.Rect(50, 120, 250, 50)
+        pygame.draw.rect(screen, (0, 255, 0), start_button_rect)  # Green color for the start button
+        screen.blit(start_text, (start_button_rect.x + 10, 130))
+
+        return button_rect, start_button_rect  # Return both button rectangles for click check
+    else:
+        return button_rect 
+is_start_phase = True  # Initial state
+num_disks = 5
+
 
 # Функция для проверки нажатия на кнопку числа дисков
 def check_disk_button(pos):
-    if 50 <= pos[0] <= 150 and 50 <= pos[1] <= 100:
+    if 50 <= pos[0] <= 200 and 50 <= pos[1] <= 100:
         return True
     return False
 
-# Определение главного цикла игры
+is_start_phase = True  # Initial state
+num_disks = 5
+towers = create_towers(num_disks)
+
 def main():
     clock = pygame.time.Clock()
-    num_disks = 5  # Default number of disks
+    global is_start_phase
+    global num_disks
     global towers
-    towers = create_towers(num_disks) 
-   
 
     while True:
         for event in pygame.event.get():
@@ -89,30 +108,48 @@ def main():
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_pos = pygame.mouse.get_pos()
-                if check_disk_button(mouse_pos):
-                    num_disks = (num_disks % 10) + 1  # Изменение числа дисков при клике на кнопку
-                    towers = create_towers(num_disks)  # Обновить башни при изменении числа дисков
 
-            # Обработка событий клавиш
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_1:
-                    move_disk(0, 1)
-                elif event.key == pygame.K_2:
-                    move_disk(0, 2)
-                elif event.key == pygame.K_3:
-                    move_disk(1, 0)
-                elif event.key == pygame.K_4:
-                    move_disk(1, 2)
-                elif event.key == pygame.K_5:
-                    move_disk(2, 0)
-                elif event.key == pygame.K_6:
-                    move_disk(2, 1)
+                if not is_start_phase and 50 <= mouse_pos[0] <= 200 and 50 <= mouse_pos[1] <= 100:
+                    # Check if the mouse is in the disk selection button area only when it's not the start phase
+                    num_disks += 1  # Increment the number of disks
+                    if num_disks > 10:  # Limit the number of disks to 10
+                        num_disks = 1
+
+                    # Update towers with the new number of disks
+                    towers = create_towers(num_disks)
+                elif is_start_phase:
+                    button_rect, start_button_rect = draw_disk_button(num_disks, is_start_button=True)
+                    if start_button_rect.collidepoint(mouse_pos):
+                        is_start_phase = False
 
         screen.fill(WHITE)
         draw_towers()
-        draw_disk_button(num_disks)  # Отрисовка кнопки
+        if is_start_phase:
+            draw_disk_button(num_disks, is_start_button=True)  # Draw the start button
+        else:
+            draw_disk_button(num_disks, is_start_button=False)  # Draw the disk selection button
+            # Perform other actions based on user input (e.g., move disk)
+
         pygame.display.flip()
         clock.tick(30)
+                    
+
 
 if __name__ == "__main__":
     main()
+
+
+
+"""if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_1:
+                        move_disk(0, 1)
+                    elif event.key == pygame.K_2:
+                        move_disk(0, 2)
+                    elif event.key == pygame.K_3:
+                        move_disk(1, 0)
+                    elif event.key == pygame.K_4:
+                        move_disk(1, 2)
+                    elif event.key == pygame.K_5:
+                        move_disk(2, 0)
+                    elif event.key == pygame.K_6:
+                        move_disk(2, 1)"""
